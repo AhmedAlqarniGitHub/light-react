3737b34d9f224f7b7bbc62bb9e35c9fb26528212
ahmed <ahmedalg4321@gmail.com>
2024-12-13 03:22:58 +0300

    status updated , send and recieve is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
-import XmppManager from "./services/xmppIndex.js";
+import XmppManager, { eventList } from "./services/xmppIndex.js";
 
 const theme = createTheme();
 const xmppManager = new XmppManager();
@@ -11,24 +11,20 @@ const xmppManager = new XmppManager();
 function App() {
   const [contacts, setContacts] = useState([]);
 
-  // Subscribe to contact updates from XmppManager
-  xmppManager.addEventListener("CONTACT_STATUS_CHANGED", (updatedContacts) => {
-    setContacts(updatedContacts);
-  });
+  // Use useEffect to ensure we add the listener only once
+  useEffect(() => {
+    xmppManager.addEventListener(eventList.CONTACT_STATUS_CHANGED, (updatedContacts) => {
+      setContacts([...updatedContacts]); // Ensure a new array reference
+    });
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={<LoginPage xmppManager={xmppManager} />}
-          />
-          <Route
-            path="/home"
-            element={<HomePage client={xmppManager} contacts={contacts} />}
-          />
+          <Route path="/" element={<LoginPage xmppManager={xmppManager} />} />
+          <Route path="/home" element={<HomePage xmppManager={xmppManager} contacts={contacts} />} />
         </Routes>
       </Router>
     </ThemeProvider>
